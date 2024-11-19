2f6b8da57a9ca2682c9d73cac71542aa2c841d08
someuser921 <luciferm921@gmail.com>
2024-11-19 20:44:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-// Функция для получения параметров из URL
-function getUrlParameter(name) {
-    const params = new URLSearchParams(window.location.search);
-    return params.get(name);
-}
-
-// Получение telegram_id и telegram_nick из URL
-const telegram_id = getUrlParameter("telegram_id");
-const telegram_nick = getUrlParameter("telegram_nick");
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("https://someuser921.pythonanywhere.com/api/get_dates")
-        .then(response => response.json())
-        .then(dates => {
-            let select = document.getElementById("date-select");
-            select.innerHTML = ""; // Очистка старых опций, если они есть
-            dates.forEach(date => {
-                let option = document.createElement("option");
-                option.value = date;
-                option.textContent = date;
-                select.appendChild(option);
-            });
-        })
-        .catch(error => {
-            console.error("Ошибка при загрузке дат:", error);
-        });
-});
-
-// Обработчик для кнопки "Оплатить онлайн"
-function payOnline() {
-    handleRegistration("online");
-}
-
-// Обработчик для кнопки "Оплатить на месте"
-function payOnSite() {
-    handleRegistration("on_site");
-}
-
 // Функция для обработки регистрации
 function handleRegistration(paymentMethod) {
     const name = document.getElementById("name-input").value;
@@ -59,11 +21,12 @@ function handleRegistration(paymentMethod) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, phone, date, paymentMethod, telegram_id, telegram_nick })
     })
-    .then(response => {
-        if (response.ok) {
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === "success") {
             document.getElementById("confirmation-message").textContent = "Вы успешно записаны на игру!";
         } else {
-            document.getElementById("confirmation-message").textContent = "Произошла ошибка. Попробуйте еще раз.";
+            document.getElementById("confirmation-message").textContent = data.message; // Показываем сообщение об ошибке
         }
     })
     .catch(error => {
